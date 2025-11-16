1ade65fdcd11893085669a13c40c1a0075958cdf
Karshoul <Khuongmingo@gmail.com>
2025-11-16 15:16:20 +0700

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,3 @@
-// frontend/src/App.jsx
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardPage from "./pages/DashboardPage";
-<<<<<<< HEAD
-import { Toaster } from "sonner";
-import CategoriesPage from "./pages/CategoriesPage";
-
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {/* DashboardPage là trang chính */}
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/categories" element={<CategoriesPage/>}/>
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
-    </>
-=======
 import CategoriesPage from "./pages/CategoriesPage";
 import ProductsPage from "./pages/ProductsPage";
 import TablesPage from "./pages/TablesPage";
@@ -37,7 +16,7 @@ function App() {
         <Route path="/reports" element={<ReportsPage />} />
       </Routes>
     </BrowserRouter>
->>>>>>> ecf26401ac69046ee97d397a781e29f813dc4d3d
+    
   );
 }
 
